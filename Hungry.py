01f16b19549af6e7c624e836a40285b4762c11a6
Mubarak L <mubaraklbsc@gmail.com>
2017-02-27 00:54:11 +0530

    water

--- Hungry.py
+++ Hungry.py
@@ -1,1 +1,10 @@
-print "Hi Helloooo"
+Hungry = raw_input("Are you hungry?")
+if Hungry=="yes":
+    print "eat pizza"
+    print "eat burger"
+    print "eat samosa"
+else:
+    print "do your job"
+thirsty = raw_input("are you thirsty?")
+if thirsty=="yes":
+    print "drink water"
